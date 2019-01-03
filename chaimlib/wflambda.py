f1b1cb7ffedfeff5afcd8a8b00ea3a77b44ec057
Chris Allison <chris.allison@bgch.co.uk>
2019-01-03 16:56:45 +0000

    adding license header

--- chaimlib/wflambda.py
+++ chaimlib/wflambda.py
@@ -1,3 +1,24 @@
+#
+# Copyright (c) 2018, Centrica Hive Ltd.
+#
+#     This file is part of chaim.
+#
+#     chaim is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     chaim is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with chaim.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Cloned from https://github.com/wavefrontHQ/wavefront-lambda-python
+# Parts of which are under the Apache License 2.0 (see apache-license-2.0)
+#
 import os
 from datetime import datetime
 from wavefront_pyformance.wavefront_reporter import WavefrontDirectReporter
