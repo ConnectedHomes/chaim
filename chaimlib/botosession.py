0dec62e3af29e460a3ee66c5189b7a272b1141f0
Chris Allison <chris.allison@bgch.co.uk>
2019-01-04 05:02:58 +0000

    changed botosession to accept keyword args

--- chaimlib/botosession.py
+++ chaimlib/botosession.py
@@ -17,17 +17,18 @@
 #     along with chaim.  If not, see <http://www.gnu.org/licenses/>.
 """Base Module for creating a default session with boto3 to AWS"""
 import boto3
-from botocore.exceptions import ClientError
 import chaimlib.glue as glue
 
 log = glue.log
 
+
 class NoCreds(Exception):
     pass
 
+
 class BotoSession():
     """Base class to create a default boto3 session"""
-    def __init__(self,theprofile=None,accessid=None,secretkey=None,usedefault=False,stoken=None):
+    def __init__(self, profile=None, accesskey=None, secretkey=None, stoken=None):
         """sets up a default connection to AWS.
 
         Either the profile or accessid/secretkey are required
@@ -36,8 +37,9 @@ class BotoSession():
 
         Keyword Arguments:
             profile - the ~/.aws/credentials profile to use.
-            accessid - the aws access key id to use (along with the secret key).
+            accesskey - the aws access key id to use (along with the secret key).
             secretkey - the aws secret access key to use (along with the access key id).
+            stoken - the aws session token to use
             usedefault - use the default context to create a session.
 
         Environment Variables:
@@ -45,37 +47,34 @@ class BotoSession():
             AWS_SECRET_ACCESS_KEY
             AWS_SESSION_TOKEN
         """
-        self.client=None
-        self.profile=None
-        self.usekeys=False
-        self.usedefault=usedefault
-        if self.usedefault==False:
-            if theprofile==None and (accessid!=None and secretkey!=None):
-                self.accessid=accessid
-                self.secretkey=secretkey
-                self.stoken=stoken
-                self.usekeys=True
-            elif theprofile!=None:
-                self.profile=theprofile
-            else:
-                log.error("Required credentials not supplied, cannot continue")
-                raise NoCreds("Either the profile or one (or both) keys are missing.")
+        self.client = None
+        self.profile = None
+        self.usekeys = False
+        self.kwargs = None
+        self.usedefault = True
+        if profile is None and (accesskey is not None and secretkey is not None):
+            self.kwargs = {"aws_access_key_id": accesskey, "aws_secret_access_key": secretkey,
+                           "aws_session_token": stoken}
+            self.usekeys = True
+            self.usedefault = False
+        elif profile is not None:
+            self.profile = profile
+            self.usedefault = False
 
     def newSession(self):
-        if self.profile==None:
+        if self.profile is None:
             return boto3.session.Session()
         else:
             return boto3.session.Session(profile_name=self.profile)
 
-    def newClient(self,service='iam'):
+    def newClient(self, service='iam'):
         try:
-            if self.usedefault:
-                session=self.newSession()
-                self.client=session.client(service)
-            elif self.usekeys:
-                self.client=boto3.client(service,aws_access_key_id=self.accessid,aws_secret_access_key=self.secretkey,aws_session_token=self.stoken)
+            if self.usekeys:
+                self.client = boto3.client(service, **self.kwargs)
             else:
-                session=self.newSession()
-                self.client=session.client(service)
+                session = self.newSession()
+                self.client = session.client(service)
         except Exception as e:
-            log.error("Failed to create a {} client, exception was: {}".format(service,e))
+            msg = "Failed to create a {} client. {}: {}".format(service, type(e).__name__, e)
+            log.error(msg)
+            raise NoCreds(msg)
