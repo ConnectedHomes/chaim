a707862a3fc6d3de836c67ae98dcd24d364fd9c1
Chris Allison <chris.allison@bgch.co.uk>
2019-01-04 19:43:18 +0000

    simplifying entry code

--- lambda/chaim-entry/app.py
+++ lambda/chaim-entry/app.py
@@ -1,9 +1,6 @@
 import chaimlib.glue as glue
 import chaimlib.chaim as chaim
 from chaimlib.envparams import EnvParam
-from chaimlib.chaim import SlackRcvFail
-from chaimlib.snsclient import SnsClient
-from chaimlib.commandparse import CommandParse
 from chalice import Chalice
 
 
@@ -21,31 +18,19 @@ def slackreq():
     try:
         with open("version", "r") as vfn:
             version = vfn.read()
-        kwargs = {}
+        config = {}
         ep = EnvParam()
-        kwargs["environment"] = ep.getParam("environment")
-        kwargs["useragent"] = "slack"
-        kwargs["apiid"] = app.current_request.context["apiId"]
-        reqbody = chaim.begin(app.current_request.raw_body.decode(), **kwargs)
-        log.debug("request: {}".format(reqbody))
-        log.debug("asking for responseurl")
-        cp = CommandParse(reqbody, blankbody=True)
-        responseurl = cp.responseurl
-        log.debug("response url is set to {}".format(responseurl))
-        if len(responseurl) == 0 or responseurl is None:
-            msg = "no response url in reqbody"
-            log.error("{}: {}".format(msg, reqbody))
-            raise(SlackRcvFail(msg))
-        log.debug("sending to sns: {}".format(reqbody))
-        snstopicarn = ep.getParam("SNSTOPIC", decode=True)
-        snsc = SnsClient()
-        snsc.publishToSns(snstopicarn, reqbody)
-        verstr = "chaim-slack-" + kwargs["environment"] + " " + version
+        config["environment"] = ep.getParam("environment")
+        config["useragent"] = "slack"
+        config["apiid"] = app.current_request.context["apiId"]
+        chaim.snsPublish(ep.getParam("SNSTOPIC"),
+                         chaim.begin(app.current_request.raw_body.decode(), **config))
+        verstr = "chaim-slack-" + config["environment"] + " " + version
         return chaim.output(None, "{}\n\nPlease wait".format(verstr))
     except Exception as e:
-        msg = "slackreq: {}: {}".format(type(e).__name__, e)
-        log.error(msg)
-        return chaim.output(msg)
+        emsg = "slackreq: {}: {}".format(type(e).__name__, e)
+        log.error(emsg)
+        return chaim.output(emsg)
 
 
 @app.route('/')
