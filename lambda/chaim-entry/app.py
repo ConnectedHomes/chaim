ab5061cdd0a471dff30ffa78444bcfd6a1648a1a
Chris Allison <chris.allison@bgch.co.uk>
2019-04-05 00:48:36 +0100

    entry working

--- lambda/chaim-entry/app.py
+++ lambda/chaim-entry/app.py
@@ -47,9 +47,13 @@ def slackreq():
         config["environment"] = ep.getParam("environment")
         config["useragent"] = "slack"
         config["apiid"] = app.current_request.context["apiId"]
-        chaim.snsPublish(ep.getParam("SNSTOPIC"),
-                         chaim.begin(app.current_request.raw_body.decode(), **config))
-        verstr = "chaim-slack-" + config["environment"] + " " + version
+        rbody = chaim.begin(app.current_request.raw_body.decode(), **config)
+        chaim.snsPublish(ep.getParam("SNSTOPIC"), rbody)
+        verstr = "chaim-slack"
+        if config["environment"] == "prod":
+            verstr += " " + version
+        else:
+            verstr += "-" + config["environment"] + " " + version
         return chaim.output(None, "{}\n\nPlease wait".format(verstr))
     except Exception as e:
         emsg = "slackreq: {}: {}".format(type(e).__name__, e)
