6fdbb5fa68e65395299560ba017eae0182f526b9
Chris Allison <chris.allison@bgch.co.uk>
2019-08-24 11:14:36 +0100

    create slackmap table

---
+++ sql/slackmap-create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE if not exists `slackmap` (
+  `userid` int(11) NOT NULL,
+  `slackid` varchar(12) NOT NULL,
+  `workspaceid` varchar(12) NOT NULL,
+  PRIMARY KEY (`userid`,`slackid`,`workspaceid`),
+  KEY `sfk_awsuser` (`userid`),
+  CONSTRAINT `sfk_awsuser` FOREIGN KEY (`userid`) REFERENCES `awsusers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
