296af208b95b4371e3cbee6b5d69fb28c392c0ae
Chris Allison <chris.allison@bgch.co.uk>
2019-09-03 16:09:06 +0100

    client version bump

--- client/cca/__init__.py
+++ client/cca/__init__.py
@@ -17,7 +17,7 @@
 #     along with chaim.  If not, see <http://www.gnu.org/licenses/>.
 #
 majorv = 2
-minorv = 3
-buildv = 4
+minorv = 4
+buildv = 0
 __version__ = str(majorv) + "." + str(minorv) + "." + str(buildv)
 __version_info__ = [majorv, minorv, buildv]
