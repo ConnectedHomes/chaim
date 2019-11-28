f8b5560e4e5e97e76ae9b1d20df31da633ef1dee
Chris Allison <chris.allison@bgch.co.uk>
2019-11-28 05:59:15 +0000

    working context manager v0.5.0

--- module/chaim/__init__.py
+++ module/chaim/__init__.py
@@ -17,7 +17,7 @@
 #     along with chaim.  If not, see <http://www.gnu.org/licenses/>.
 #
 majorv = 0
-minorv = 0
-buildv = 1
+minorv = 5
+buildv = 0
 __version__ = str(majorv) + "." + str(minorv) + "." + str(buildv)
 __version_info__ = [majorv, minorv, buildv]
